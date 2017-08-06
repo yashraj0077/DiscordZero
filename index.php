b34f19e22178a492c9e3f4c17b7a25b743d6b21e
samdenty99 <sam@samdd.me>
2017-08-06 22:33:12 +0100

    Basic authentication

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	if(isset($_POST['u']) && isset($_POST['p'])) {
+		echo $_POST['u'];
+	} else if(isset($_GET['u']) && isset($_GET['p'])) {
+	    echo $_GET['u'];
+	} else {
+		include ("login.html");
+	}
+?>
