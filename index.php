b8418436ddde10b47ade8e5045b812ec8518d489
samdenty99 <sam@samdd.me>
2017-08-07 01:41:51 +0100

    Updated to use filesystem

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 	if(isset($_POST['u']) && isset($_POST['p'])) {
-		echo $_POST['u'];
+		$username = $_POST['u'];
+		$password = $_POST['p'];
+		goto token;
 	} else if(isset($_GET['u']) && isset($_GET['p'])) {
-	    echo $_GET['u'];
+	    $username = $_GET['u'];
+	    $password = $_GET['p'];
+	    goto token;
 	} else {
 		include ("login.html");
-	} 
+		exit;
+	}
+	token:
+	echo $username . " " . $password;
+	if (!file_exists("api/" . $username)) {
+	    mkdir("api/" . $username, 0777);
+	    $api = fopen("api/" . $username . "/" . $password . ".json", "w") or die("Unable to open file!");
+	    fwrite($api, "message");
+	    fclose($api);
+	}
 ?>
