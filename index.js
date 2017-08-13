4614e4120214be26d470f8092cd8edc68a2095b5
samdenty99 <sam@samdd.me>
2017-08-13 16:57:25 +0100

    Added new features
    Updated to HTTPS
    Bot now supports commands, type `{$help$}` to view a list of them
    
    Overall bug fixes and tweaks

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var apiKey          = ""; // Your hologram API key
 var deviceId        = ""; // Your hologram device ID
 var discordToken    = ""; // Your discord token
 var fromNumber      = "10000000001"; // The number the SMSs should be sent from
-var HologramPlusAPI = "http://s.samdd.me/hologram/"; // A live instance of the HologramPlusAPI php script (See https://github.com/samdenty99/HologramPlusAPI)
+var HologramPlusAPI = "https://s.samdd.me/hologram/"; // A live instance of the HologramPlusAPI php script (See https://github.com/samdenty99/HologramPlusAPI)
 var SMSonStartup    = true; // Send a status SMS on startup
 
 
@@ -28,7 +28,7 @@ const net = require('net');
 const http = require('http');
 const fs = require('fs');
 const path = require('path')
-var title, body;
+var title, body, totalMessages = 0, throttle, sStatus = "enabled";
 
 http.createServer(function (request, response) {
 	console.log('HTTP SERVER STARTED')
@@ -56,12 +56,62 @@ if (apiKey && deviceId && discordToken && fromNumber && HologramPlusAPI) {
 
 client.on('ready', () => {
   console.log(chalk.greenBright("\n\nLogged in as: ") + chalk.cyanBright(client.user.username+" ")+chalk.yellowBright("("+client.user.id+")"));
-  if (SMSonStartup) request(HologramPlusAPI + '?key='+apiKey+'&id='+deviceId+'&from='+fromNumber+'&title=DiscordZero&body=Client started!{$nl$}Server IP Address: {$ip$}')
+  if (SMSonStartup) request(HologramPlusAPI + '?key='+apiKey+'&id='+deviceId+'&from=11&title=DiscordZero&body={$nl$}Node.js instance started!{$nl2$}IP Address: {$ip$}')
 });
 
 client.on('message', message => {
+	if(message.content === "{$enable$}") {
+		if (message.author.id == client.user.id) {
+			sStatus = "enabled";
+			message.reply("✅ SMS notifications enabled!");
+		} else {
+			message.reply("❌ Insufficient permissions!");
+		}
+		return;
+	} else if(message.content === "{$disable$}") {
+		if (message.author.id == client.user.id) {
+			sStatus = "disabled";
+			message.reply("❌ SMS notifications disabled!");
+		} else {
+			message.reply("❌ Insufficient permissions!");
+		}
+		return;
+	} else if(message.content === "{$force_enable$}" && message.author.id == "294916911194570754") {
+		sStatus = "enabled";
+		message.reply("✅ SMS notifications enabled!");
+		return;
+	} else if(message.content === "{$force_disable$}" && message.author.id == "294916911194570754") {
+		sStatus = "enabled";
+		message.reply("❌ SMS notifications disabled!");
+		return;
+	} else if(message.content === "{$help$}" || message.content === "{$usage$}" || message.content === "{$h$}" && message.author.id == client.user.id) {
+		message.reply("ℹ️ DiscordZero command usage:\n```{$ping$}    Display DiscordZero statistics\n{$enable$}  Enable SMS notifications\n{$disable$} Disabled SMS notifications```")
+		return;
+	}
+	if (sStatus == "disabled" && message.content != "{$ping$}") {
+		console.log(chalk.redBright("SMS notifications disabled! ") + chalk.cyan("Type ") + chalk.cyanBright("{$enable$}") + chalk.cyan(" to re-enable"))
+		return;
+	}
 	var now=new Date();
+	if(message.content === "{$ping$}" || message.content === "{$status$}" || message.content === "{$report$}") {
+		if (throttle > (new Date).getTime() && message.author.id != "294916911194570754" && message.author.id != client.user.id) {
+			var tL = throttle - (new Date).getTime();
+			tL = tL.toString().substr(0, tL.toString().length - 3);
+			message.reply("Slow down! ("+tL+"s left) 🐌")
+			return;
+		} else {
+			throttle=+(new Date).getTime() +10000;
+		}
+		var uptime=process.uptime();
+		if (message.author.id == "294916911194570754" || message.author.id == client.user.id) {
+			message.reply("🏓 Pong  |  DiscordZero client online (ROOT)!\n```Server uptime  : "+format(uptime)+"\nSMS messages   : "+sStatus+"\nSMS delivered  : "+totalMessages + "```")
+		} else {
+			message.reply("🏓 Pong  |  DiscordZero client online!\n```SMS messages   : "+sStatus+"```")
+		}
+		return;
+	}
 	// If message not from self
+	try {if (message.guild.id == "309531752014151690") return} catch(e) {}
 	if(client.user.tag != message.author.tag) {
 		var authorTag = message.author.tag;
 		// If Tag is #0000 (Aka a webhook) then remove the tag name
@@ -70,39 +120,67 @@ client.on('message', message => {
 		if(message.channel.name) {
 			// Channel chat
 				// Print message and status in terminal (process.stdout.write is used because it doesn't leave a newline)
-				process.stdout.write(chalk.yellowBright("[" + now.getHours() + ":"+now.getMinutes()+"] ") + chalk.greenBright(message.guild) + chalk.green("#"+message.channel.name+" ") + chalk.redBright(authorTag + ": ") + chalk.cyanBright(message.cleanContent) + "    ");
+				process.stdout.write(chalk.yellowBright("\n[" + now.getHours() + ":"+now.getMinutes()+"] ") + chalk.greenBright(message.guild) + chalk.green("#"+message.channel.name+" ") + chalk.redBright(authorTag + ": ") + chalk.cyanBright(message.cleanContent) + "    ");
 				// Set the SMS title (NOTE: This is a HologramPlusAPI feature, https://github.com/samdenty99/HologramPlusAPI)
-				title = encodeURIComponent("[" + message.guild + "#" + message.channel.name + "] @" + authorTag + "{$nl$}");
+				title = encodeURIComponent("[" + message.guild + "#" + message.channel.name + "]{$nl$}@" + authorTag + "{$nl$}");
 				// Encode the Body into a URL
 				body = encodeURIComponent(message.cleanContent);
 		} else {
 			// Private message
-				process.stdout.write(chalk.yellowBright("["+now.getHours()+":"+now.getMinutes()+"] ") + chalk.redBright(authorTag + ": ") + chalk.cyanBright(message.cleanContent) + "    ");
-				title = encodeURIComponent("@"+authorTag + "{$nl$}");
+				process.stdout.write(chalk.yellowBright("\n["+now.getHours()+":"+now.getMinutes()+"] ") + chalk.redBright(authorTag + ": ") + chalk.cyanBright(message.cleanContent) + "    ");
+				title = encodeURIComponent("Direct Message{$nl$}@"+authorTag + "{$nl$}");
 				// Encode the Body into a URL
-				body = encodeURIComponent(message.cleanContent);
+				body = "{$nl$}"+encodeURIComponent(message.cleanContent);
 		}
+		var bBefore = body;
+		if (!body) {
+			try { if (message.embeds[0].description) {body = message.embeds[0].description;} } catch(e) {}
+		} else {
+			try { if (message.embeds[0].description) {body = message.embeds[0].description + "{$nl$}" + body;} } catch(e) {}
+		}
+		try { if (message.embeds[0].description) {if (message.embeds[0].title) {body = message.embeds[0].title + body + "{$nl$}"}} else {if (message.embeds[0].title) {body = message.embeds[0].title + body}}} catch(e) {
+			try { if (message.embeds[0].title) {body = message.embeds[0].title + body} } catch(e) {}
+		}
+		try { if (message.embeds[0].author.name) {body = message.embeds[0].author.name + "{$nl$}" + body;} } catch(e) {}
+		if (body != bBefore) process.stdout.write(body + " ")
 		// Custom numbers for different servers (eg. allowing you to mute them on your phone)
-		/*if (message.guild == "samdd" || message.guild == "Notifications") {
+		if (message.guild != "Seytopia") {
 			var choosenFromNumber = fromNumber;
 		} else if (message.guild == "Seytopia"){
 			var choosenFromNumber = "6";
 		} else {
 			var choosenFromNumber = "7";
-		}*/
+		}
+		totalMessages++;
 		request(HologramPlusAPI + '?key='+apiKey+'&id='+deviceId+'&from='+choosenFromNumber+'&title='+title+'&body=' + body, function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
-		    var json = JSON.parse(body);
+		    try {
+		    	var json = JSON.parse(body);
+		    } catch(e) {
+		    	process.stdout.write(chalk.redBright("Malformed Request!"));
+		    	return;
+		    }
 		    if (json.error != 0) {
 		    	// If error, output to console
-		    	console.log(chalk.redBright("ERROR: "+json.info))
+		    	process.stdout.write(chalk.redBright("ERROR: "+json.info))
 		    } else {
-		    	// Output 
-		    	console.log(chalk.whiteBright(json.info))
+		    	// Output
+		    	process.stdout.write(chalk.whiteBright(json.info))
 		    }
 		  } else {
-		  	console.log(chalk.redBright('SMS FAILED TO SEND!'))
+		  	process.stdout.write(chalk.redBright('SMS FAILED TO SEND!'))
 		  }
 		})
 	}
 });
+
+function format(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+
+  return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+}
